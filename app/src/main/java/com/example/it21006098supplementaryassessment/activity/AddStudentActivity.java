1f700800ef95aace5375f7f1891e2ede8e637be5
IT21006098 <it21006098@my.sliit.lk>
2022-12-08 17:27:08 +0530

    SQLite database initialized

--- app/src/main/java/com/example/it21006098supplementaryassessment/activity/AddStudentActivity.java
+++ app/src/main/java/com/example/it21006098supplementaryassessment/activity/AddStudentActivity.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.it21006098supplementaryassessment.R;
+import com.example.it21006098supplementaryassessment.db.DBHelper;
+import com.example.it21006098supplementaryassessment.model.Student;
 
 public class AddStudentActivity extends AppCompatActivity {
 
+    EditText edName, edCourse, edMobile, edTotal, edPaid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+        edName = findViewById(R.id.edName);
+        edCourse = findViewById(R.id.edCourse);
+        edMobile = findViewById(R.id.edMobile);
+        edTotal = findViewById(R.id.edTotal);
+        edPaid = findViewById(R.id.edPaid);
     }
 
     public void showAll(View view) {
@@ -21,5 +31,25 @@ public class AddStudentActivity extends AppCompatActivity {
     }
 
     public void save(View view) {
+        String name = edName.getText().toString().trim();
+        String course = edCourse.getText().toString().trim();
+        String mobile = edMobile.getText().toString().trim();
+        String total = edTotal.getText().toString().trim();
+        String paid = edPaid.getText().toString().trim();
+
+        DBHelper dbHelper = new DBHelper(AddStudentActivity.this);
+
+        Student s = new Student(name, course, mobile, Integer.parseInt(total), Integer.parseInt(paid));
+
+        long result = dbHelper.addStudent(s);
+
+        if( result > 0)
+        {
+            Toast.makeText(this, "Saved " + result, Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Failed " + result, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/it21006098supplementaryassessment/activity/StudentListActivity.java
+++ app/src/main/java/com/example/it21006098supplementaryassessment/activity/StudentListActivity.java
@@ -1,16 +1,39 @@
 package com.example.it21006098supplementaryassessment.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.it21006098supplementaryassessment.R;
+import com.example.it21006098supplementaryassessment.db.DBHelper;
+import com.example.it21006098supplementaryassessment.model.Student;
+
+import java.util.ArrayList;
 
 public class StudentListActivity extends AppCompatActivity {
+
+    TextView tvTotal;
+    RecyclerView recyclerView;
+
+    ArrayList<Student> students;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list);
+
+        tvTotal = findViewById(R.id.tvTotal);
+
+        DBHelper dbHelper = new DBHelper(this);
+
+        students = dbHelper.getAllStudents();
+        tvTotal.setText("Total Students : " + students.size());
+
+        for( Student s : students)
+        {
+            System.out.println(s.getName());
+        }
     }
 }
